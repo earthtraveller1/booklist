bfd6a6a72820b93082383f1c73a099dc358ebc82
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-11-01 19:00:59 -0400

    Got rid of the uneccessary main class.

--- Sources/main.swift
+++ Sources/main.swift
@@ -12,23 +12,18 @@ func index(request: Request) async throws -> View {
     return try await request.view.render("index", Context(books: books))
 }
 
-@main
-public class BookList {
-    public static func main() throws {
-        let app = Application()
+let app = Application()
 
-        app.views.use(.leaf)
-        app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-        app.databases.use(.sqlite(.file("books.db")), as: .sqlite)
+app.views.use(.leaf)
+app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+app.databases.use(.sqlite(.file("books.db")), as: .sqlite)
 
-        app.migrations.add(CreateBooks())
-        try app.group("api") { api in 
-            try api.register(collection: BookController())
-        }
-
-        app.get(use: index)
-
-        try app.run()
-        app.shutdown()
-    }
+app.migrations.add(CreateBooks())
+try app.group("api") { api in 
+    try api.register(collection: BookController())
 }
+
+app.get(use: index)
+
+try app.run()
+app.shutdown()
